5b906a8f3f9c06d67e320ba288e17c9c75e6d983
jangni <90706827@163.com>
2018-03-20 00:52:06 +0800

    netty 1

--- src/main/java/com/jangni/netty1/client/TimeClient.java
+++ src/main/java/com/jangni/netty1/client/TimeClient.java
@@ -9,7 +9,6 @@ public class TimeClient {
     public static void main(String[] args){
         String host = "127.0.0.1";
         int port = 8080;
-
         new Thread(new TimeClientHandle(host,port),"TimeClient-001").start();
     }
 }
